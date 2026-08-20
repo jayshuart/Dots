dc84b595fb53dd380bd3c6437bbf0863926c3c96
jashuart <joelshuart@gmail.com>
2026-08-20 02:47:50 -0400

    scoring anim attempts

--- Dots/Assets/Scripts/UI/Score.cs
+++ Dots/Assets/Scripts/UI/Score.cs
@@ -7,11 +7,13 @@ public class Score : MonoBehaviour
     [SerializeField] private Image backingImage;
     [SerializeField] private Image bar;
     [SerializeField] private TMP_Text scoreText;
+    private int _numScore = 0;
+    private Animator _animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,11 +29,25 @@ public class Score : MonoBehaviour
 
     public void SetScoreText(int score)
     {
+        PlayAnimScore(score);
+        _numScore = score;
         scoreText.text = score.ToString();
     }
-    
+
     public void SetIsTurn(bool isTurn)
     {
         bar.gameObject.SetActive(!isTurn);
+    }
+    
+    public void PlayAnimScore(int score)
+    {
+        //if (score <= _numScore) { return; } 
+
+        int diff = score - _numScore;
+        // int streak = diff > 0 ?
+        //     _animator.GetInteger("Streak") : 0;
+        int streak = 0; //remove if streask are used
+        _animator.SetInteger("Streak", streak + diff);
+        _animator.SetTrigger("Increment");
     }
 }
